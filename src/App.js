8b3a76e24ddf670f598ce7e31f7dc889ec4bea8a
nu-takoe <simon300897@yandex.ru>
2022-03-30 02:55:12 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Button, Divider, Modal } from 'antd';
-import { CaretRightOutlined } from '@ant-design/icons'
+import { ArrowsAltOutlined, CaretRightOutlined, PauseCircleOutlined, ShrinkOutlined } from '@ant-design/icons'
 import './App.css';
 import { useMachine } from '@xstate/react';
 import stateMachine from './stateMachine';
@@ -8,14 +8,34 @@ import ReactPlayer from 'react-player';
 function App() {
   const [state, send] = useMachine(stateMachine)
   const open = state.matches('open')
+  const size = state.context.size
+  const play = state.context.play
+
+  console.log(state.context)
+
+  const styles = {
+    mini: {
+      top: 10,
+    }
+  }
 
   return (
     <>
       <div className='play-window'>
-        <Button type='primary' icon={<CaretRightOutlined />}  onClick={()=> send("TOGGLE_ON")}/>
+        <Button type='primary' icon={<CaretRightOutlined />} onClick={() => send("TOGGLE_ON")} />
       </div>
-      <Modal visible={open} onCancel={() => send("TOGGLE_OFF")}>
-        <ReactPlayer/>
+
+      <Modal visible={open} onCancel={() => send("TOGGLE_OFF")} footer={false} bodyStyle={{paddingTop: 50, width: '100%', height: '70%'}} style={!size && styles.mini} width={size ? "1080px" : "480px"}>
+
+        <ReactPlayer playing={play} width='100%' height={size ? "720px" : "360px"} url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+
+        <Divider />
+
+        <div style={{ textAlign: 'right', width: '100%', }}>
+          <Button type='primary' icon={size ? <ShrinkOutlined /> : <ArrowsAltOutlined />} onClick={() => send("SIZE")} />
+          <Button shape='circle' style={{ marginLeft: 10 }} icon={<PauseCircleOutlined />} onClick={() => send("MODE")} />
+        </div>
+
       </Modal>
     </>
   );
--- src/stateMachine.js
+++ src/stateMachine.js
@@ -1,67 +1,78 @@
-import { createMachine } from "xstate";
+import { assign, createMachine } from "xstate";
 
-const stateMachine = createMachine(
-    {
-        id: "machine",
-        initial: "close",
-        states: {
-            close: {
-                on: {
-                    TOGGLE_ON: {
-                        target: "open"
+const stateMachine = createMachine({
+    id: "machine",
+    initial: "close",
+    context: {
+        //true - большой, false - маленький
+        size: true,
+        //false - пауза, true - проигрывается
+        play: false,
+    },
+    states: {
+        close: {
+            exit: assign({ play: true }),
+            on: {
+                TOGGLE_ON: {
+                    target: "open"
+                }
+            }
+        },
+        open: {
+            exit: assign({ play: false }),
+            type: "parallel",
+            states: {
+                mode: {
+                    initial: "play",
+                    states: {
+                        pause: {
+                            exit: assign({ play: true }),
+                            on: {
+                                MODE: {
+                                    target: "play"
+                                }
+                            }
+                        },
+                        play: {
+                            exit: assign({ play: false }),
+                            on: {
+                                MODE: {
+                                    target: "pause"
+                                }
+                            }
+                        }
+                    }
+                },
+                size: {
+                    initial: "big",
+                    states: {
+                        mini: {
+                            exit: assign({ size: true }),
+                            on: {
+                                SIZE: {
+                                    target: "big"
+                                }
+                            }
+                        },
+                        big: {
+                            exit: assign({ size: false }),
+                            on: {
+                                SIZE: {
+                                    target: "mini"
+                                }
+                            }
+                        }
                     }
                 }
             },
-            open: {
-                // "type": "parallel",
-                // "states": {
-                //     "mode": {
-                //         "initial": "pause",
-                //         "states": {
-                //             "pause": {
-                //                 "on": {
-                //                     "MODE": {
-                //                         "target": "play"
-                //                     }
-                //                 }
-                //             },
-                //             "play": {
-                //                 "on": {
-                //                     "MODE": {
-                //                         "target": "pause"
-                //                     }
-                //                 }
-                //             }
-                //         }
-                //     },
-                //     "size": {
-                //         "initial": "mini",
-                //         "states": {
-                //             "mini": {
-                //                 "on": {
-                //                     "SIZE": {
-                //                         "target": "big"
-                //                     }
-                //                 }
-                //             },
-                //             "big": {
-                //                 "on": {
-                //                     "SIZE": {
-                //                         "target": "mini"
-                //                     }
-                //                 }
-                //             }
-                //         }
-                //     }
-                // },
-                on: {
-                    TOGGLE_OFF: {
-                        target: "close"
-                    }
+            on: {
+                TOGGLE_OFF: {
+                    target: "close"
                 }
             }
         }
     }
+}
 )
 
 export default stateMachine
